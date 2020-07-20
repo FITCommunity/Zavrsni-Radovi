d434e7c437df9c7bf8b7c9e049602755b225421c
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-07-20 14:46:26 +0200

    Update setup script so created folders in /Radovi/ can be tracked in git

--- setup.sh
+++ setup.sh
@@ -16,9 +16,19 @@ if [ -e Radovi/$1/ ]; then
   echo "Folder Radovi/$1/ već postoji!"
 else
   mkdir Radovi/$1/
+
   mkdir Radovi/$1/'dr. sc. Elmir Babović'
+  echo >> Radovi/$1/'dr. sc. Elmir Babović'/.gitkeep
+ 
   mkdir Radovi/$1/'dr. sc. Denis Mušić'
+  echo >> Radovi/$1/'dr. sc. Denis Mušić'/.gitkeep
+
   mkdir Radovi/$1/'dr. sc. Jasmin Azemović'
+  echo >> Radovi/$1/'dr. sc. Jasmin Azemović'/.gitkeep
+  
   mkdir Radovi/$1/'dr. sc. Nina Bijedić'
+  echo >> Radovi/$1/'dr. sc. Nina Bijedić'/.gitkeep
+
   mkdir Radovi/$1/'dr. sc. Emina Junuz'
+  echo >> Radovi/$1/'dr. sc. Emina Junuz'/.gitkeep
 fi
