f871bb88c9dde41ba12985a7515ec010b52e4bd3
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-07-20 17:49:18 +0200

    Simplify repository structure

---
+++ .github/CONTRIBUTING.md
@@ -0,0 +1,5 @@
+# Contributing
+
+* Otvoriti issue `New Entry` i navesti sve informacije
+* Dodati novi rad u odgovarajući `README.md` file u formatu `* Naslov završnog rada - Ime Prezime Studenta` 
+* [Opcionalno] Dodati u isti folder u kojem je i `README.md` u koji je dodan rad i `.pdf` primjerak završnog rada
--- setup.sh
+++ setup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [ -e Indeks/$1/ ]; then
-  echo "Folder Indeks/$1/ već postoji!"
-else
-  mkdir Indeks/$1/
-  echo >> Indeks/$1/'dr. sc. Elmir Babović.md'
-  echo >> Indeks/$1/'dr. sc. Denis Mušić.md'
-  echo >> Indeks/$1/'dr. sc. Jasmin Azemović.md'
-  echo >> Indeks/$1/'dr. sc. Nina Bijedić.md'
-  echo >> Indeks/$1/'dr. sc. Emina Junuz.md'
-fi
-
 
 if [ -e Radovi/$1/ ]; then
   echo "Folder Radovi/$1/ već postoji!"
@@ -18,17 +7,17 @@ else
   mkdir Radovi/$1/
 
   mkdir Radovi/$1/'dr. sc. Elmir Babović'
-  echo >> Radovi/$1/'dr. sc. Elmir Babović'/.gitkeep
+  echo >> Radovi/$1/'dr. sc. Elmir Babović'/README.md
  
   mkdir Radovi/$1/'dr. sc. Denis Mušić'
-  echo >> Radovi/$1/'dr. sc. Denis Mušić'/.gitkeep
+  echo >> Radovi/$1/'dr. sc. Denis Mušić'/README.md
 
   mkdir Radovi/$1/'dr. sc. Jasmin Azemović'
-  echo >> Radovi/$1/'dr. sc. Jasmin Azemović'/.gitkeep
+  echo >> Radovi/$1/'dr. sc. Jasmin Azemović'/README.md
   
   mkdir Radovi/$1/'dr. sc. Nina Bijedić'
-  echo >> Radovi/$1/'dr. sc. Nina Bijedić'/.gitkeep
+  echo >> Radovi/$1/'dr. sc. Nina Bijedić'/README.md
 
   mkdir Radovi/$1/'dr. sc. Emina Junuz'
-  echo >> Radovi/$1/'dr. sc. Emina Junuz'/.gitkeep
+  echo >> Radovi/$1/'dr. sc. Emina Junuz'/README.md
 fi
